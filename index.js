7a7229044e548f42c6b548242e2263d05a42b9c0
FannyEstefaniadev <fannyestefania8@gmail.com>
2023-04-08 17:54:48 -0300

    PreEntrega1+Ramos

---
+++ index.js
@@ -0,0 +1,56 @@
+const precio1 = 1300;
+const precio2 = 800;
+const precio3 = 950;
+const precio4 = 1000;
+
+var precioTotal = 0;
+
+var precioElemento = document.getElementById("precio");
+
+function boton1() {
+  precioTotal += precio1;
+  mostrarPrecio();
+}
+
+function boton2() {
+  precioTotal += precio2;
+  mostrarPrecio();
+}
+
+function boton3() {
+  precioTotal += precio3;
+  mostrarPrecio();
+}
+
+function boton4() {
+  precioTotal += precio4;
+  mostrarPrecio();
+}
+
+function mostrarPrecio() {
+  precioElemento.textContent = "Precio total: $" + precioTotal;
+}
+
+var listaElemento = document.getElementById("lista");
+
+function agregarPedido() {
+  var confirmar = false;
+  while (!confirmar) {
+    var pedidos = prompt("Por favor confirme su pedido:");
+    if (pedidos != null && pedidos != "") {
+      var pedidosArray = pedidos.split(",");
+      for (var i = 0; i < pedidosArray.length; i++) {
+        var pedido = pedidosArray[i].trim();
+        if (pedido != "") {
+          var li = document.createElement("li");
+          li.textContent = pedido;
+          listaElemento.appendChild(li);
+        }
+      }
+    } else {
+      alert("Por favor confirme su pedido.");
+      continue;
+    }
+    confirmar = confirm("¿Está seguro de su compra?");
+  }
+}
